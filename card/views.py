3da42fbab481a2d5083f04aec4781673ad490ab3
KasiaWrzalka <katarzyna@wrzalka.com>
2018-09-03 14:13:41 +0200

    Paginator

--- card/views.py
+++ card/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response, render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Card
@@ -7,18 +8,24 @@ def index(request):
     return render(request, 'card/index.html', {})
 
 def CardList(request, sort):
-    cards = Card.objects.filter(created_date__lte=timezone.now()).order_by('id')
-    print(sort)
     if sort == 'name':
-        cards = Card.objects.filter(created_date__lte=timezone.now()).order_by('name')
+        cards_list = Card.objects.filter(created_date__lte=timezone.now()).order_by('name')
     elif sort == 'countR':
-        cards = sorted(Card.objects.filter(created_date__lte=timezone.now()),
+        cards_list = sorted(Card.objects.filter(created_date__lte=timezone.now()),
                        key=lambda i: i.carditem_set.all().count())
     elif sort == 'countM':
-        cards = sorted(Card.objects.filter(created_date__lte=timezone.now()),
+        cards_list = sorted(Card.objects.filter(created_date__lte=timezone.now()),
                        key=lambda i: i.carditem_set.all().count())[::-1]
     else:
-        cards = Card.objects.filter(created_date__lte=timezone.now()).order_by('id')
+        cards_list = Card.objects.filter(created_date__lte=timezone.now()).order_by('id')
+
+    cards_list = [i for i in cards_list if i.carditem_set.all().count() > 0]
+
+    paginator = Paginator(cards_list, 2)
+
+    page = request.GET.get('page')
+    cards = paginator.get_page(page)
+
     return render(request, 'card/card_list.html', {'cards': cards})
 
 def CardItems(request, card_id):
