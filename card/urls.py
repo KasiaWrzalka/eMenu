225bdfb8c2b44b41e59db6d7f2d9a174514cc6f9
KasiaWrzalka <katarzyna@wrzalka.com>
2018-09-03 12:23:28 +0200

    Sort list of cards by: id name numbers of items

--- card/urls.py
+++ card/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('card_list/', views.CardList, name='card_list'),
+    path('<slug:sort>/card_list/', views.CardList, name='card_list'),
     path('<int:card_id>/items/', views.CardItems, name='card_items'),
 ]
--- card/views.py
+++ card/views.py
@@ -6,11 +6,19 @@ from .models import Card
 def index(request):
     return render(request, 'card/index.html', {})
 
-def CardList(request):
+def CardList(request, sort):
     cards = Card.objects.filter(created_date__lte=timezone.now()).order_by('id')
-    c2 = Card.objects.filter(id=3)
-    for i in c2:
-        print((i.carditem_set.all().count()))
+    print(sort)
+    if sort == 'name':
+        cards = Card.objects.filter(created_date__lte=timezone.now()).order_by('name')
+    elif sort == 'countR':
+        cards = sorted(Card.objects.filter(created_date__lte=timezone.now()),
+                       key=lambda i: i.carditem_set.all().count())
+    elif sort == 'countM':
+        cards = sorted(Card.objects.filter(created_date__lte=timezone.now()),
+                       key=lambda i: i.carditem_set.all().count())[::-1]
+    else:
+        cards = Card.objects.filter(created_date__lte=timezone.now()).order_by('id')
     return render(request, 'card/card_list.html', {'cards': cards})
 
 def CardItems(request, card_id):
